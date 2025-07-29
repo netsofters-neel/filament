847fc74356e59273bc2a50309f17113bd4aac626
netsofters-neel <126561179+netsofters-neel@users.noreply.github.com>
2025-07-29 12:15:26 +0530

    Update crud.php

--- crud.php
+++ crud.php
@@ -1,1 +1,10 @@
+Example need to create {Employee} CRUD
+
+- php artisan make:model {Employee} -m (Generate Modle and table schma)
+- php artisan migrate
+- Make Model Fillable
+    - go to -> app/Models/Employee.php
+    - protected $fillable = ['name', 'email', 'phone', 'status'];  (change filled)
+- php artisan make:filament-resource {Employee} --generate
+
 
